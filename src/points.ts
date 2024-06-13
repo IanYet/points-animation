68684dee47694f6a4a32515ab58eff320938139e
IanYet <yt2015.97@gmail.com>
2024-06-14 00:09:49 +0800

    球与chaos的计算

--- src/points.ts
+++ src/points.ts
@@ -20,9 +20,24 @@ export function initPoints() {
 				// const edge = [0, 9]
 				// if (!edge.includes(x) && !edge.includes(y) && !edge.includes(z)) continue
 
+				//cube
 				pos0[i * 3 + 0] = x - 4.5
 				pos0[i * 3 + 1] = y - 4.5
 				pos0[i * 3 + 2] = z - 4.5
+
+				//chaos
+				pos1[i * 3 + 0] = 40 * Math.random() - 20
+				pos1[i * 3 + 1] = 40 * Math.random() - 20
+				pos1[i * 3 + 2] = 40 * Math.random() - 20
+
+				// sphere
+				const theta = (Math.PI * Math.random() * 9) / 10
+				const phi = (Math.PI * 2 * Math.random() * 10) / 10
+				const r = 5 * Math.random() ** (1 / 2)
+
+				pos2[i * 3 + 0] = r * Math.sin(theta) * Math.cos(phi)
+				pos2[i * 3 + 1] = r * Math.sin(theta) * Math.sin(phi)
+				pos2[i * 3 + 2] = r * Math.cos(theta)
 
 				colors[i * 3 + 0] = (x + 1) / 11
 				colors[i * 3 + 1] = (y + 1) / 11
@@ -34,11 +49,13 @@ export function initPoints() {
 
 	const geo = new BufferGeometry()
 	geo.setAttribute('position', new BufferAttribute(pos0, 3))
+	geo.setAttribute('position1', new BufferAttribute(pos1, 3))
+	geo.setAttribute('position2', new BufferAttribute(pos2, 3))
 	geo.setAttribute('color', new BufferAttribute(colors, 3))
 
 	const mat = new PointsMaterial({
 		vertexColors: true,
-		size: 1,
+		size: 0.1,
 	})
 
 	const points = new Points(geo, mat)
